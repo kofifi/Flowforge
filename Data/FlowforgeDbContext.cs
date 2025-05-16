026aac7abf373d1ddcd03b07728eb0332fa65c0f
Konrad Firlej <kofirlej@gmail.com>
2025-05-16 18:00:24 +0200

    Initial commit

---
+++ Data/FlowforgeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Flowforge.Models;
+
+namespace Flowforge.Data;
+
+public class FlowforgeDbContext : DbContext
+{
+    public FlowforgeDbContext(DbContextOptions<FlowforgeDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Workflow> Workflows { get; set; }
+    public DbSet<Block> Blocks { get; set; }
+    public DbSet<SystemBlock> SystemBlocks { get; set; }
+    public DbSet<BlockConnection> BlockConnections { get; set; }
+    public DbSet<WorkflowVariable> WorkflowVariables { get; set; }
+    public DbSet<WorkflowRevision> WorkflowRevisions { get; set; }
+    public DbSet<WorkflowExecution> WorkflowExecutions { get; set; }
+}
---
+++ Models/WorkflowVariable.cs
@@ -0,0 +1,12 @@
+﻿namespace Flowforge.Models;
+
+public class WorkflowVariable
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string? DefaultValue { get; set; }
+
+    public int WorkflowId { get; set; }
+    public Workflow Workflow { get; set; } = null!;
+}
